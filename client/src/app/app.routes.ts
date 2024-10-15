0fe9c76c52fe2eaf79e364aebd18ca6a6029d2be
Wathila Dewnaka Weerakoon Karunathilake <165549226+wathilaDewnaka@users.noreply.github.com>
2024-10-15 18:57:55 +0530

    Update app.routes.ts

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { LoginComponent } from './auth/components/login/login.component';
 import { ForgetComponent } from './auth/components/forget/forget.component';
 
 export const routes: Routes = [
+  { path: "", component: LoginComponent },
     { path: "login", component: LoginComponent },
     { path: "register", component: SignupComponent },
     { path: "login/forget", component: ForgetComponent },
